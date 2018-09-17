226a2e8c48f888aa70e0caca43e73f9fc502b009
Kevin Perkins <kevcorper@gmail.com>
2018-09-17 15:02:11 -0400

    listen/accept connections

--- index.c
+++ index.c
@@ -1,21 +1,58 @@
+// Server side C program to demonstrate HTTP Server programming
+#include <stdio.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
 
-if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+#define PORT 8080
+int main(int argc, char const *argv[])
 {
-    perror(“cannot create socket”); 
-    return 0; 
-}
-
-struct sockaddr_in address;
-const int PORT = 8080; //Where the clients can reach at
-/* htonl converts a long integer (e.g. address) to a network representation */ 
-/* htons converts a short integer (e.g. port) to a network representation */ 
-memset((char *)&address, 0, sizeof(address)); 
-address.sin_family = AF_INET; 
-address.sin_addr.s_addr = htonl(INADDR_ANY); 
-address.sin_port = htons(PORT); 
-if (bind(server_fd,(struct sockaddr *)&address,sizeof(address)) < 0) 
-{ 
-    perror(“bind failed”); 
-    return 0; 
+    int server_fd, new_socket; long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    char *hello = "HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 12\n\nHello world!";
+    
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        
+        char buffer[30000] = {0};
+        valread = read( new_socket , buffer, 30000);
+        printf("%s\n",buffer );
+        write(new_socket , hello , strlen(hello));
+        printf("------------------Hello message sent-------------------");
+        close(new_socket);
+    }
+    return 0;
 }
